4e7c0b04d090cfce1c527df2d2070080aafaa0da
Samuel Ojo <ifegbenga@gmail.com>
2022-05-14 08:46:18 +0100

    frontend stake integrations

--- frontend/pages/Stake.js
+++ frontend/pages/Stake.js
@@ -10,31 +10,68 @@ function Stake() {
       <div className={styles.root}>
         <Nav />
         <div className={styles.container}>
-          <select className={styles.list}>
-            <option disabled selected className={styles.li}>
-              Select Token to stake
-            </option>
-            <option className={styles.li}>Token</option>
-            <option className={styles.li}>Token</option>
-            <option className={styles.li}>Token</option>
-          </select>
-          <div className={styles.amount}>
-            <input
-              className={styles.placeholder}
-              type={Number}
-              placeholder={"Amount"}
-            />
-            <button className={styles.max}>max</button>
-          </div>
-          <label className={styles.label}>Choose staking period</label>
-          <div>
-            <button className={styles.days}>7 days</button>
-            <button className={styles.days}>30 days</button>
-            <button className={styles.days}>90 days</button>
-          </div>
-          <div className={styles.stakeBTN}>
-            <button className={styles.stake}>stake</button>
-          </div>
+          <form>
+            <select className={styles.list}>
+              <option disabled className={styles.li}>
+                Select Token to stake
+              </option>
+              <option className={styles.li}>Token</option>
+              <option className={styles.li}>Token</option>
+              <option className={styles.li}>Token</option>
+            </select>
+            <div className={styles.amount}>
+              <input
+                className={styles.placeholder}
+                type="number"
+                placeholder="Amount"
+                min="0"
+                max="20000"
+              />
+              <button className={styles.max}>max</button>
+            </div>
+            <p className={styles.label}>Choose staking period</p>
+            <div className={styles.period}>
+              <div className={styles["period__row"]}>
+                <input
+                  className={styles["period__input"]}
+                  type="radio"
+                  id="period_01"
+                  name="period"
+                  value="7"
+                />
+                <label className={styles["period__label"]} for="period_01">
+                  <p>7 days</p>
+                </label>
+              </div>
+              <div className={styles["period__row"]}>
+                <input
+                  className={styles["period__input"]}
+                  type="radio"
+                  id="period_02"
+                  name="period"
+                  value="30"
+                />
+                <label className={styles["period__label"]} for="period_02">
+                  <p>30 days</p>
+                </label>
+              </div>
+              <div className={styles["period__row"]}>
+                <input
+                  className={`${styles["period__input"]} ${styles["period__period_03"]}`}
+                  type="radio"
+                  id="period_03"
+                  name="period"
+                  value="90"
+                />
+                <label className={styles["period__label"]} for="period_03">
+                  <p>90 days</p>
+                </label>
+              </div>
+            </div>
+            <div className={styles.stakeBTN}>
+              <button className={styles.stake}>stake</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
